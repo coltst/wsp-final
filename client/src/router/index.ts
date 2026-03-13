d98b2ed2530af129383d4bd9493a044b978e0bc3
coltst <230728993+coltst@users.noreply.github.com>
2026-03-12 23:13:34 -0400

    remove boilerplate & adopt mvvm

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,8 +1,15 @@
+import HomePage from '@/views/HomePage.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'root',
+      component: HomePage
+    }
+  ],
 })
 
 export default router
