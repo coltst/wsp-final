910fa263885a008765fb3d40241f26d0a08475e7
coltst <230728993+coltst@users.noreply.github.com>
2026-03-12 23:19:38 -0400

    move script into script tag

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,8 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import { RouterView } from 'vue-router'
+</script>
 
 
-import { RouterView } from 'vue-router'
 
 <template>
   <RouterView />
