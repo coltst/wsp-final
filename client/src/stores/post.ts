3022deed53f0cace7e50fc413f27567755333361
coltst <230728993+coltst@users.noreply.github.com>
2026-03-15 16:08:18 -0400

    temporary add post function

--- client/src/stores/post.ts
+++ client/src/stores/post.ts
@@ -5,8 +5,19 @@ import dataPosts from '../data/posts.json'
 
 export const usePostStore = defineStore('post', () => {
   const posts = ref<Post[]>(dataPosts.posts)
+  const postNumber = ref<number>(11); // will be determined by DB
   function addPost(post: NewPost) {
-
+    posts.value.push({
+      id: postNumber.value, // this will be determined by the DB later
+      title: post.title,
+      body: post.body,
+      tags: post.tags,
+      reactions: {"likes": 0, "dislikes": 0},
+      views: 0,
+      user: post.user,
+      comments: []
+    });
+    postNumber.value += 1;
   }
   function addComment(post_id: number, author: string, body: string) {
     for (const post of posts.value) {
