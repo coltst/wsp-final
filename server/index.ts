1d66633f5b9146313f0733d3155acbf0c2e80260
coltst <230728993+coltst@users.noreply.github.com>
2026-04-15 14:52:50 -0400

    working http server already!

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,14 @@
-console.log("hello world");
+import express from "express";
+
+const PORT = 3000;
+const HOSTNAME = "localhost";
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("It works!");
+});
+
+app.listen(PORT, () => {
+    console.log(`Listening: http://${HOSTNAME}:${PORT}`);
+});
