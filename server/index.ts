3a76a3f808193ed1e76b002223a2cfb852522d14
coltst <230728993+coltst@users.noreply.github.com>
2026-04-25 10:43:35 -0400

    serve frontend from backend

--- server/index.ts
+++ server/index.ts
@@ -1,15 +1,30 @@
 import express from "express";
+import { config } from "dotenv";
+config();
 
-const PORT = 3000;
-const HOSTNAME = "localhost";
+const PORT = process.env.PORT ?? 3000;
+const HOSTNAME = process.env.BIND ?? "localhost";
+const STATIC_DIR = process.env.SERVE ?? "../client/dist";
 
 const app = express();
 
+app.use((_req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE")
+    res.setHeader("Access-Control-Allow-Headers", "*")
+    next()
+});
 app.use(express.json());
+app.use(express.static(STATIC_DIR));
 
-app.get("/", (req, res) => {
+app.get("/api/v1/test", (req, res) => {
     res.send("It works!");
 });
+
+app.use( (err: Error, _req: express.Request, res: express.Response, _next: express.NextFunction) => {
+    console.log(err);
+    res.status(500).send("Error")
+})
 
 app.listen(PORT, () => {
     console.log(`Listening: http://${HOSTNAME}:${PORT}`);
