5be144c9fabff5f42b2cb92fc78d452396937e66
coltst <230728993+coltst@users.noreply.github.com>
2026-04-26 17:48:10 -0400

    fix typo

--- server/models/users.ts
+++ server/models/users.ts
@@ -27,7 +27,7 @@ async function getAll(params: PagingRequest) {
 
 async function getById(id: number) {
     const db = connect();
-    const result = await db.from("users").select("*").eq("id", id).single();
+    const result = await db.from("users").select("*").eq("userid", id).single();
     if (result.error) {
         throw {status: 404, message: "User not found"};
     }
