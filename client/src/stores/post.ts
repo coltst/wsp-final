42dccae5e4ac69201fbee7b2bbc9ec0fa6255fd6
coltst <230728993+coltst@users.noreply.github.com>
2026-04-27 09:41:15 -0400

    Update post store to use backend
    The post store functions as the controller, with the API service kind of acting as the model

--- client/src/stores/post.ts
+++ client/src/stores/post.ts
@@ -1,38 +1,63 @@
 import { ref } from 'vue'
 import { defineStore } from 'pinia'
-import type { NewPost, Post } from '@/types'
-import dataPosts from '../data/posts.json'
+import { useSessionStore } from './session'
+//import type { NewPost, Post } from '@/types'
+//import dataPosts from '../data/posts.json'
+import type { DataEnvelope, DataListEnvelope, Post } from "../../../server/types";
 
 export const usePostStore = defineStore('post', () => {
-  const posts = ref<Post[]>(dataPosts.posts)
-  const postNumber = ref<number>(11); // will be determined by DB
-  function addPost(post: NewPost) {
-    posts.value.push({
-      id: postNumber.value, // this will be determined by the DB later
-      title: post.title,
-      body: post.body,
-      tags: post.tags,
-      reactions: {"likes": 0, "dislikes": 0},
-      views: 0,
-      user: post.user,
-      comments: []
-    });
-    postNumber.value += 1;
+  const session = useSessionStore();
+  const posts = ref<Post[]>([]);
+
+  // The store functions as the controller
+  async function loadPosts() {
+    const data = await session.api<DataListEnvelope<Post>>("/post");
+    posts.value = data.data;
   }
-  function addComment(post_id: number, author: string, body: string) {
+
+  async function getPost(id: number) {
+    return session.api<DataEnvelope<Post>>(`/post/${id}`);
+  }
+
+  async function addPost(post: Omit<Post, "postID">) {
+    const data = await session.api<DataEnvelope<Post>>('/post/new', post);
+    posts.value.push(data.data);
+    return data;
+  }
+
+  async function updatePost(id: number, post: Omit<Post, "postID">) {
+    const data = await session.api<DataEnvelope<Post>>(`/post/${id}`, post, {
+      method: 'PATCH',
+    })
+    const index = posts.value.findIndex((p) => p.postID === id)
+    if (index !== -1) {
+      posts.value[index] = data.data
+    }
+    return data
+  }
+
+  async function deletePost(id: number) {
+    const data = await session.api<DataEnvelope<Post>>(`/post/${id}`, null, {
+      method: 'DELETE',
+    })
+    const index = posts.value.findIndex((p) => p.postID === id)
+    if (index !== -1) {
+      posts.value.splice(index, 1)
+    }
+    return data
+  }
+
+  function addComment(post_id: number, _author: string, _body: string) {
     for (const post of posts.value) {
-      if (post.id === post_id) {
-        post.comments.push({
+      if (post.postID === post_id) {
+        /*post.comments.push({
           id: -1, // will be determined by DB later
           body: body,
           user: author
-        });
+        });*/
       }
     }
   }
-  function deletePost(post_id: number) {
-    posts.value = posts.value.filter((post) => post.id !== post_id);
-  }
 
-  return { posts, addPost, deletePost, addComment }
+  return { posts, addPost, deletePost, loadPosts, getPost, updatePost, addComment }
 })
