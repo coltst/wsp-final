49e5a9c5394726b72ed95d685c3de7c4478618ab
coltst <230728993+coltst@users.noreply.github.com>
2026-04-27 12:55:14 -0400

    remove extraneous imports

--- client/src/views/ProfilePage.vue
+++ client/src/views/ProfilePage.vue
@@ -1,10 +1,7 @@
 <script setup lang="ts">
 
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { faArrowRightFromBracket } from '@fortawesome/free-solid-svg-icons'
 import { useSessionStore } from '@/stores/session';
 import UserInfo from '@/components/UserInfo.vue';
-import type { Post } from '@/types';
 import { usePostStore } from '@/stores/post';
 const sessionStore = useSessionStore();
 
