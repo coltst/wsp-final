7fa2d750dace928bf0cac6ed681c7f9df5a5af99
coltst <230728993+coltst@users.noreply.github.com>
2026-04-28 19:11:22 -0400

    remove extraneous comment that probably had related code deleted already

--- server/controllers/post.ts
+++ server/controllers/post.ts
@@ -22,7 +22,6 @@ app.get("/", async (req, res) => {
         res.send(response);
     })
     .post("/new", async (req, res) => {
-        // TODO: remove this when we do JWT
         const newPost = await create(req.body, req.user?.userid ?? -1);
         const response: DataEnvelope<Post> = {
             data: newPost,
--- server/controllers/reply.ts
+++ server/controllers/reply.ts
@@ -32,7 +32,6 @@ app.get("/", async (req, res) => {
         res.send(response);
     })
     .post("/new", async (req, res) => {
-        // TODO: remove this when we do JWT
         const newReply = await create(req.body, req.user?.userid ?? -1);
         const response: DataEnvelope<Reply> = {
             data: newReply,
